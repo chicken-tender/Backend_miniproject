d25990ea0727370f9f8e0a2ac3a176bffcd8c4db
chicken-tender <ygm815@naver.com>
2023-04-24 18:35:54 +0900

    :tada: Spring Boot First Commit

---
+++ src/main/java/com/kh/Backend_miniproject/BackendMiniprojectApplication.java
@@ -0,0 +1,13 @@
+package com.kh.Backend_miniproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMiniprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendMiniprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kh/Backend_miniproject/BackendMiniprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.Backend_miniproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMiniprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
