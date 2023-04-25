044eeba2d9708b012127989cfa5ddc63ba6cf7cb
chicken-tender <ygm815@naver.com>
2023-04-25 14:05:36 +0900

    :construction_worker: MainController getTopWriter 추가

--- src/main/java/com/kh/Backend_miniproject/controller/MainController.java
+++ src/main/java/com/kh/Backend_miniproject/controller/MainController.java
@@ -1,9 +1,22 @@
 package com.kh.Backend_miniproject.controller;
+import com.kh.Backend_miniproject.dao.MainDao;
+import com.kh.Backend_miniproject.vo.TopWriterVO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class MainController {
-
+    @GetMapping("/main/topwriter")
+    public ResponseEntity<List<TopWriterVO>> getTopWritersInfo() {
+        MainDao mdao = new MainDao();
+        List<TopWriterVO> list = mdao.getTopWriters();
+        return new ResponseEntity<>(list, HttpStatus.OK);
+    }
 }
