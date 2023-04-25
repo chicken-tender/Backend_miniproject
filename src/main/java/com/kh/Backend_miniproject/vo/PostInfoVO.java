25874335e4946e6b17a2ccdc28ea00071b037ec2
chicken-tender <ygm815@naver.com>
2023-04-25 12:18:59 +0900

    :art: 구조 잡기..

--- src/main/java/com/kh/Backend_miniproject/vo/PostInfoVO.java
+++ src/main/java/com/kh/Backend_miniproject/vo/PostInfoVO.java
@@ -1,4 +1,14 @@
 package com.kh.Backend_miniproject.vo;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class PostInfoVO {
+    private String title;
+    private String nickName;
+    private String pfImg;
+    private int viewCount;
+    private int commentCount;
 }
