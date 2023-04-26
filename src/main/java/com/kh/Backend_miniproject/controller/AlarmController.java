06310dc15c6cbe6132111f4d49e8b6ed7c1a6240
chicken-tender <ygm815@naver.com>
2023-04-26 14:31:36 +0900

    :memo: AlarmController 완성

--- src/main/java/com/kh/Backend_miniproject/controller/AlarmController.java
+++ src/main/java/com/kh/Backend_miniproject/controller/AlarmController.java
@@ -1,10 +1,22 @@
 package com.kh.Backend_miniproject.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import com.kh.Backend_miniproject.dao.AlarmDAO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class AlarmController {
+    @GetMapping("/author-email")
+    public ResponseEntity<String> fetchAuthorEmailByPostNum(@RequestParam int postNum) {
+        AlarmDAO adao = new AlarmDAO();
+        String authorEmail = adao.getAuthorEmailByPostNum(postNum);
 
+        if(authorEmail != null) {
+            return new ResponseEntity<>(authorEmail, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
