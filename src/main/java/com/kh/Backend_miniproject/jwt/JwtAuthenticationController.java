8362188aca21dc9033fad8199709917679a54fe3
chicken-tender <ygm815@naver.com>
2023-05-16 18:41:26 +0900

    :bulb: CrossOrigin 추가

--- src/main/java/com/kh/Backend_miniproject/jwt/JwtAuthenticationController.java
+++ src/main/java/com/kh/Backend_miniproject/jwt/JwtAuthenticationController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class JwtAuthenticationController {
     @Autowired
     private MyUserDetailsService userDetailsService;
--- src/main/java/com/kh/Backend_miniproject/jwt/UserController.java
+++ src/main/java/com/kh/Backend_miniproject/jwt/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
     @GetMapping("/user")
     public ResponseEntity<List<MembersVO>> getUserInfo() {
